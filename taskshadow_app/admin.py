cb09e4cb3292623f51b2ff75b3c0069001a3ef65
David Bonser <david@codapt.co.uk>
2025-01-13 17:34:58 +0000

    set up admin panel and started creating app foundations

--- taskshadow_app/admin.py
+++ taskshadow_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import TaskShadowTodo
 
-# Register your models here.
+admin.site.reguster(TaskShadowTodo)
--- taskshadow_app/views.py
+++ taskshadow_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as django_logout
 
-# Create your views here.
+#Model imports
+from .models import TaskShadowTodo
+
+@login_required
+def todo_list(request):
+    todos = TaskShadowTodo.objects.filter(user=request.user)
+    return render(request, 'todo_list.html', {'todos': todos})
+
+#Add To-do List View
+@login_required
+def add_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        TaskShadowTodo.objects.create(user=request.user, title=title)
+    return redirect('todo_list')
+
+#Mark Complete To-do List View
+@login_required
+def mark_complete(request, todo_id):
+    todo = TaskShadowTodo.objects.get(pk=todo_id)
+    todo.completed =not todo.completed
+    todo.save()
+    return redirect('todo_list')
