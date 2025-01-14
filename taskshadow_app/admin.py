2734d44a96660c7000f278c21b1742b215f73ca7
David Bonser <david@codapt.co.uk>
2025-01-14 15:59:33 +0000

    creating forms and login functions, foundations almost done

--- taskshadow_app/admin.py
+++ taskshadow_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import TaskShadowTodo
 
-admin.site.reguster(TaskShadowTodo)
+admin.site.register(TaskShadowTodo)
--- taskshadow_app/views.py
+++ taskshadow_app/views.py
@@ -22,7 +22,47 @@ def add_todo(request):
 #Mark Complete To-do List View
 @login_required
 def mark_complete(request, todo_id):
-    todo = TaskShadowTodo.objects.get(pk=todo_id)
+    todo = TaskShadowTodo.objects.get(id=todo_id, user=request.user)
     todo.completed =not todo.completed
     todo.save()
     return redirect('todo_list')
+
+#Delete To-do List View
+@login_required
+def delete_todo(request, todo_id):
+    todo = TaskShadowTodo.objects.get(id=todo_id, user=request.user)
+    todo.delete()
+    return redirect('todo_list')
+
+#Register View
+@login_required
+def register(request):
+    if request.method == 'POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('todo_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+#Login View
+def user_login(request):
+    if request.method == 'POST':
+        form = CustomUserAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('todo_list')
+    else:
+        form = CustomUserAuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+#Logout View
+@login_required
+def logout(request):
+    django_logout(request)
+    return redirect('login')
