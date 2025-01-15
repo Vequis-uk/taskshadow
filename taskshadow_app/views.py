7cadbbf1d9106ca1ad2d75f25c1a77c6423a01d0
David Bonser <david@codapt.co.uk>
2025-01-15 13:28:11 +0000

    views and forms completed

--- taskshadow_app/views.py
+++ taskshadow_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as django_logout
+from .forms import CustomUserCreationForm, CustomUserAuthenticationForm
 
 #Model imports
 from .models import TaskShadowTodo
