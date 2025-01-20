e601223986b2b090492d5cada3d13264ea7ace90
David Bonser <david@codapt.co.uk>
2025-01-20 15:38:08 +0000

    updated settings.py for deployment, added whitenoise

--- taskshadow_project/settings.py
+++ taskshadow_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure--5v=sx$hq45183aodb9u!xy=)l+wyx1x6@3w)@*f$nmp^1!)%^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEBUG' in os.environ Todo look at this
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -104,7 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'taskshadow_app/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
