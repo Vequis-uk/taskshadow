791b7b2ba9b3d91641c0bba026deed85a13b4e7b
David Bonser <david@codapt.co.uk>
2025-01-27 14:22:37 +0000

    updated how secret key is stored

--- taskshadow_project/settings.py
+++ taskshadow_project/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--5v=sx$hq45183aodb9u!xy=)l+wyx1x6@3w)@*f$nmp^1!)%^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEBUG' in os.environ Todo look at this
 DEBUG = False
 
 ALLOWED_HOSTS = ['taskshadow-20be08825c40.herokuapp.com', 'localhost', '127.0.0.1']
