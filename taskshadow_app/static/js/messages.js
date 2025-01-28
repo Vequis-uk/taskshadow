6e38e431ed7603367291d02a48f33990513490c0
David Bonser <david@codapt.co.uk>
2025-01-28 10:48:35 +0000

    styling updates

--- taskshadow_app/static/js/messages.js
+++ taskshadow_app/static/js/messages.js
@@ -2,5 +2,5 @@
     const messages = document.querySelectorAll('.message');
     setTimeout(() => {
     messages.forEach(message => message.remove());
-}, 3000); // 3 seconds
+}, 5000); // 5 seconds
 });
